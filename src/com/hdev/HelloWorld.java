80a29c8df187c13f47e5a128480c9ba7811f51fb
YangQingHua <yqh700@163.com>
2017-10-21 16:54:49 +0800

    Initial commit

---
+++ src/com/hdev/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.hdev;
+
+/**
+ * Created by YangQingHua on 2017/10/21.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
